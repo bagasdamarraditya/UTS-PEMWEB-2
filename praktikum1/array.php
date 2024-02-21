5ef021f9cd994fb663460a0afac19394aa21242f
bagasdamarraditya <bagasdr0106@gmail.com>
2024-02-21 15:59:44 +0700

    Praktikum1

---
+++ praktikum1/array.php
@@ -0,0 +1,8 @@
+<?php
+//array indexing
+$mhs1 = ['0110123286', 'Bagas', 'Sistem Informasi', [3.5,3.7,4.0]];
+
+// Panggil array
+echo 'Nama: ' . $mhs1[1] . '<br>'; // konkatenasi string
+echo "NIM: $mhs1[0] <br>" ;
+echo "IPS semester 1: " . $mhs1 [3] [0] ;
---
+++ praktikum1/variable.php
@@ -0,0 +1,15 @@
+<?php
+//Definisi variable
+$nama = "Bagas Der";
+$umur = "18";
+$berat = "50";
+
+//memanggil variale
+echo "Nama = " . $nama;
+echo "<br/>Umur = " . $umur." tahun";
+echo "<br/>Berat = " . $berat." Kg";
+
+echo "<br/>Hello $nama apakabar";
+
+//penutup php berfungsi untuk menambahkan kode html pada kode php agar tidak eror
+?>
