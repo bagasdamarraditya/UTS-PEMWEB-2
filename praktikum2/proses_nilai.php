3d40bacf21d81ef016653b63f4a8a1dc5eb55e8b
bagasdamarraditya <bagasdr0106@gmail.com>
2024-03-01 20:47:02 +0700

    Praktikum dan tugas mandiri

--- praktikum2/proses_nilai.php
+++ praktikum2/proses_nilai.php
@@ -15,6 +15,44 @@ if($nilai_total > 55){
     $status = "Tidak Lulus";
 }
 
+// tentukan Grade nilai dari hasil ujian
+if ($nilai_total >= 85) {
+    $grade = "A";
+} elseif ($nilai_total >= 70) {
+    $grade = "B";
+} elseif ($nilai_total >= 56) {
+    $grade = "C";
+} elseif ($nilai_total >= 36) {
+    $grade = "D";
+} elseif ($nilai_total >= 0) {
+    $grade = "E";
+} else {
+    $grade = "I";
+}
+
+// tentukan predikat dari grade dari nilai ujian siswa
+switch ($grade) {
+    case 'A':
+        $predikat = "Sangat Memuaskan";
+        break;
+    case 'B':
+        $predikat = "Memuaskan";
+        break;
+    case 'C':
+        $predikat = "Cukup";
+        break;
+    case 'D':
+        $predikat = "Kurang";
+        break;
+    case 'E':
+        $predikat = "Sangat Kurang";
+        break;
+    default:
+        $predikat = "Tidak Ada";
+        break;
+}
+
+
 // tampilkan hasil form
 echo "Nama Lengkap : $nama" ;
 echo "<br>Mata Kuliah : $matkul" ;
@@ -22,4 +60,6 @@ echo "<br>Nilai UTS : $uts" ;
 echo "<br>Nilai UAS : $uas" ;
 echo "<br>Nilai Tugas Praktikum : $tugas" ;
 echo "<br>Nilai Total : $nilai_total" ;
+echo "<br>Grade : $grade";
+echo "<br>Predikat : $predikat";
 echo "<br>Status : $status" ;
