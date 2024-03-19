be1343eecc5674bf5230b9ebcd43cc792e8f83dc
bagasdamarraditya <bagasdr0106@gmail.com>
2024-03-20 00:16:00 +0700

    Tugas 4

--- Tugas4/proses_login.php
+++ Tugas4/proses_login.php
@@ -8,7 +8,6 @@ if($email == 'admin@gmail.com' && $password == '12345678'){
     header('Location: dashboard.php');
     exit(); // Pastikan untuk keluar setelah mengarahkan pengguna ke dashboard
 } else {
-    // Tampilkan pesan error melalui JavaScript
     echo "<script>
             alert('Email atau password salah.');
             window.location.href = 'index.php';
